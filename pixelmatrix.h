c68847b3b586cd7f69efb0621d243e45dd1f3049
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-07-12 00:26:54 +0200

    Use getters to access the elements of the matrix

--- pixelmatrix.h
+++ pixelmatrix.h
@@ -8,16 +8,32 @@
 class PixelMatrix
 {
 	std::vector<std::vector<Pixel>> matrix;
+
+	inline bool checkBounds(unsigned int n, unsigned int m) const;
 public:
 	const unsigned int n, m;
 
 	explicit PixelMatrix(unsigned int, unsigned int);
-	inline const std::vector<Pixel>& operator[](int) const;
+	inline Pixel& getPixel(unsigned int, unsigned int);
+	inline const Pixel& getPixel(unsigned int, unsigned int) const;
 };
 
-const std::vector<Pixel>& PixelMatrix::operator[](int i) const
+bool PixelMatrix::checkBounds(unsigned int n, unsigned int m) const
 {
-	return matrix[i];
+	return n < this->n && m < this->m;
 }
 
+Pixel& PixelMatrix::getPixel(unsigned int n, unsigned int m)
+{
+	return const_cast<Pixel&>(const_cast<const PixelMatrix *>(this)->
+				  getPixel(n, m));
+}
+
+const Pixel& PixelMatrix::getPixel(unsigned int n, unsigned int m) const
+{
+	if (!checkBounds(n, m))
+		throw std::out_of_range("Out of range error");
+
+	return matrix[n][m];
+}
 #endif // PIXELMATRIX_H
