ce2dd724a9adb53ae1bf86b515ab2b187576b155
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-07-11 14:24:04 +0200

    Change indentation to follow the coding style

--- color.h
+++ color.h
@@ -6,63 +6,63 @@
 class Color : QColor
 {
 public:
-    inline explicit Color(int, int, int, int = 255);
-    inline Color(const QColor&);
-    inline Color(QColor&&);
-    inline int red() const;
-    inline int green() const;
-    inline int blue() const;
-    inline void setRed(int r);
-    inline void setGreen(int g);
-    inline void setBlue(int b);
+	inline explicit Color(int, int, int, int = 255);
+	inline Color(const QColor&);
+	inline Color(QColor&&);
+	inline int red() const;
+	inline int green() const;
+	inline int blue() const;
+	inline void setRed(int r);
+	inline void setGreen(int g);
+	inline void setBlue(int b);
 };
 
 Color::Color(int r, int g, int b, int a) :
-    QColor(r, g, b, a)
+	QColor(r, g, b, a)
 {
 
 }
 
 Color::Color(const QColor& qcolor) :
-    QColor(qcolor)
+	QColor(qcolor)
 {
 
 }
 
 Color::Color(QColor&& qcolor) :
-    QColor(std::move(qcolor))
+	QColor(std::move(qcolor))
 {
 
 }
 
 int Color::red() const
 {
-    return QColor::red();
+	return QColor::red();
 }
 
 int Color::green() const
 {
-    return QColor::green();
+	return QColor::green();
 }
 
 int Color::blue() const
 {
-    return QColor::blue();
+	return QColor::blue();
 }
 
 void Color::setRed(int r)
 {
-    QColor::setRed(r);
+	QColor::setRed(r);
 }
 
 void Color::setGreen(int g)
 {
-    QColor::setGreen(g);
+	QColor::setGreen(g);
 }
 
 void Color::setBlue(int b)
 {
-    QColor::setBlue(b);
+	QColor::setBlue(b);
 }
 
 #endif // COLOR_H
