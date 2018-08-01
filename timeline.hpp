c5452b4c48cba2908d7e7d467fd764a8fa1bc80e
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-08-01 15:46:37 +0200

    Use map instead of vector to store KeyFrames

--- timeline.hpp
+++ timeline.hpp
@@ -1,7 +1,7 @@
 #ifndef TIMELINE_H
 #define TIMELINE_H
 
-#include <vector>
+#include <map>
 
 #include "keyframe.hpp"
 #include "grapicsstate.h"
@@ -20,10 +20,15 @@ struct return_type<GraphicsState> {
 
 template<typename T>
 class Timeline {
-	std::vector<KeyFrame<T>> keyFrames;
+	/*
+	 * Initially KeyFrames only store the duration, but when searching for
+	 * a KeyFrame in the Timeline it's useful to store when that KeyFrame
+	 * starts
+	 */
+	std::map<double, KeyFrame<T>> timeKeyFrameMap;
 
-	typename std::vector<KeyFrame<T>>::iterator
-	getIteratorForKeyFrame(double time) const;
+	typename std::map<double, KeyFrame<T>>::iterator
+	getIteratorForKeyFrame(double time);
 public:
 	void addKeyFrame(const KeyFrame<T>&);
 	typename return_type<T>::type getObject(double time);
@@ -31,37 +36,42 @@ public:
 };
 
 template<typename T>
-typename std::vector<KeyFrame<T>>::iterator
-Timeline<T>::getIteratorForKeyFrame(double time) const
+typename std::map<double, KeyFrame<T>>::iterator
+Timeline<T>::getIteratorForKeyFrame(double time)
 {
-	double playTime = 0.0;
+	auto it = timeKeyFrameMap.lower_bound(time);
+	if (it == timeKeyFrameMap.end() ||
+			it->first + it->second.getDuration() < time)
+		throw std::out_of_range("Invalid time");
 
-	for (auto it = keyFrames.begin(); it != keyFrames.end(); ++it) {
-		playTime += it->getDuration();
-
-		if (playTime > time)
-			return it;
-	}
-
-	throw std::out_of_range("Time points out of the timeline");
+	return it;
 }
 
 template<typename T>
 void Timeline<T>::addKeyFrame(const KeyFrame<T>& keyFrame)
 {
-	keyFrames.push_back(keyFrame);
+	double prevTime = 0.0;
+	double prevDuration = 0.0;
+
+	auto it = timeKeyFrameMap.rbegin();
+	if (it != timeKeyFrameMap.rend()) {
+		prevTime = it->first;
+		prevDuration = it->second.getDuration();
+	}
+
+	timeKeyFrameMap[prevTime + prevDuration] = keyFrame;
 }
 
 template<typename T>
 typename return_type<T>::type Timeline<T>::getObject(double time)
 {
-	return *getIteratorForKeyFrame(time);
+	return getIteratorForKeyFrame(time)->second.getObject();
 }
 
 template<typename T>
 typename return_type<T>::const_type Timeline<T>::getObject(double time) const
 {
-	return *getIteratorForKeyFrame(time);
+	return getIteratorForKeyFrame(time)->second.getObject();
 }
 
 /* This is not yet implemented. For GraphicsState objects we interpolate
