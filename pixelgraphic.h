e59a8e3854f0fb4ea820954ab6c0bae03f79a99c
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-08-02 16:06:22 +0200

    PixelGraphic: use unique_ptr instead of shared_ptr

--- pixelgraphic.h
+++ pixelgraphic.h
@@ -2,6 +2,7 @@
 #define PIXELGRAPHIC_H
 
 #include <vector>
+#include <memory>
 
 #include "pixelmatrix.h"
 #include "grapicsstate.h"
@@ -9,12 +10,26 @@
 
 class PixelGraphic {
 	Timeline<PixelMatrix> personalTimeline;
-	std::vector<std::pair<std::shared_ptr<PixelGraphic>,
+	std::vector<std::pair<std::unique_ptr<PixelGraphic>,
 	Timeline<GraphicsState>>> childTimelines;
 public:
 	PixelGraphic();
+	inline PixelGraphic(const PixelGraphic&);
 	inline PixelMatrix& getMatrix();
 	inline const PixelMatrix& getMatrix() const;
 };
+
+PixelGraphic::PixelGraphic(const PixelGraphic& other) :
+	personalTimeline(other.personalTimeline)
+{
+	for (const auto& it : other.childTimelines) {
+		childTimelines.push_back(
+					std::pair<std::unique_ptr<PixelGraphic>,
+					Timeline<GraphicsState>>(
+						std::make_unique<PixelGraphic>(
+							*it.first),
+						it.second));
+	}
+}
 
 #endif // PIXELGRAPHIC_H
