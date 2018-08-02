eb2eae175aca81988b6f586f73cdb52d4acddda6
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-08-02 14:38:54 +0200

    vec2: use the default constructor

--- vec2.h
+++ vec2.h
@@ -4,7 +4,6 @@
 struct vec2 {
 	double x, y;
 
-	explicit vec2(double, double);
 	inline vec2 operator+(const vec2&) const;
 	inline vec2 operator*(double) const;
 };
@@ -16,12 +15,12 @@ inline vec2 operator*(double d, const vec2& v)
 
 vec2 vec2::operator+(const vec2& v) const
 {
-	return vec2(x + v.x, y + v.y);
+	return vec2{x + v.x, y + v.y};
 }
 
 vec2 vec2::operator*(double d) const
 {
-	return vec2(d * x, d * y);
+	return vec2{d * x, d * y};
 }
 
 #endif // VEC2_H
