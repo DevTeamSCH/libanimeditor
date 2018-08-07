99a236616c551e70b2ae45c1f26259e9e39f05fa
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-08-07 09:51:17 +0200

    PixelMatrix: override getPixel for vec2

--- pixelmatrix.h
+++ pixelmatrix.h
@@ -2,8 +2,10 @@
 #define PIXELMATRIX_H
 
 #include <vector>
+#include <cmath>
 
 #include "pixel.h"
+#include "grapicsstate.h"
 
 class PixelMatrix {
 	std::vector<std::vector<Pixel>> matrix;
@@ -15,6 +17,10 @@ public:
 	explicit PixelMatrix(unsigned int, unsigned int);
 	inline Pixel& getPixel(unsigned int, unsigned int);
 	inline const Pixel& getPixel(unsigned int, unsigned int) const;
+	inline Pixel& getPixel(const vec2&);
+	inline const Pixel& getPixel(const vec2&) const;
+
+	static std::pair<int, int> roundvec2(const vec2&);
 };
 
 bool PixelMatrix::checkBounds(unsigned int n, unsigned int m) const
@@ -34,5 +40,24 @@ const Pixel& PixelMatrix::getPixel(unsigned int n, unsigned int m) const
 		throw std::out_of_range("Out of range error");
 
 	return matrix[n][m];
+}
+
+Pixel& PixelMatrix::getPixel(const vec2& v)
+{
+	return const_cast<Pixel&>(const_cast<const PixelMatrix *>(this)->
+				  getPixel(v));
+}
+
+const Pixel& PixelMatrix::getPixel(const vec2& v) const
+{
+	auto p = roundvec2(v);
+
+	return getPixel(static_cast<unsigned int>(p.first),
+			static_cast<unsigned int>(p.second));
+}
+
+std::pair<int, int> PixelMatrix::roundvec2(const vec2& v)
+{
+	return std::pair<int, int>(std::lround(v.y), std::lround(v.x));
 }
 #endif // PIXELMATRIX_H
