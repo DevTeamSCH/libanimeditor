24323d69efdf18128908f48c2505929cf704e3de
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-08-10 13:51:07 +0200

    PixelQuartet: override operator+ and operator+=

--- pixelquartet.h
+++ pixelquartet.h
@@ -11,6 +11,8 @@ public:
 	inline PixelQuartet();
 	inline Pixel& getPixel(unsigned int, unsigned int);
 	inline const Pixel& getPixel(unsigned int, unsigned int) const;
+	inline PixelQuartet operator+(const PixelQuartet&) const;
+	inline PixelQuartet& operator+=(const PixelQuartet&);
 };
 
 PixelQuartet::PixelQuartet()
@@ -30,6 +32,25 @@ const Pixel& PixelQuartet::getPixel(unsigned int n, unsigned int m) const
 		throw std::out_of_range("Out of range error");
 
 	return pixels[n][m];
+}
+
+PixelQuartet PixelQuartet::operator+(const PixelQuartet& other) const
+{
+	PixelQuartet tmp(*this);
+	for (unsigned int i = 0; i < 2; ++i) {
+		for (unsigned int j = 0; j < 2; ++j) {
+			auto& pixel = tmp.pixels[i][j];
+			pixel.setColor(pixel.getColor() +
+				       other.pixels[i][j].getColor());
+		}
+	}
+
+	return tmp;
+}
+
+PixelQuartet& PixelQuartet::operator+=(const PixelQuartet& other)
+{
+	return *this = *this + other;
 }
 
 #endif // PIXELQUARTET_H
