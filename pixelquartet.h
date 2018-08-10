a3fcb3f0fe6a2ef029c6c1a2a17594be8a22ef7b
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-08-10 13:21:26 +0200

    Create PixelQuartet class

---
+++ pixelquartet.h
@@ -0,0 +1,35 @@
+#ifndef PIXELQUARTET_H
+#define PIXELQUARTET_H
+
+#include <array>
+
+#include "pixel.h"
+
+class PixelQuartet {
+	std::array<std::array<Pixel, 2>, 2> pixels;
+public:
+	PixelQuartet();
+	Pixel& getPixel(unsigned int, unsigned int);
+	const Pixel& getPixel(unsigned int, unsigned int) const;
+};
+
+PixelQuartet::PixelQuartet()
+{
+	pixels.fill({Pixel(), Pixel()});
+}
+
+Pixel& PixelQuartet::getPixel(unsigned int n, unsigned int m)
+{
+	return const_cast<Pixel&>(const_cast<const PixelQuartet *>(this)
+				  ->getPixel(n, m));
+}
+
+const Pixel& PixelQuartet::getPixel(unsigned int n, unsigned int m) const
+{
+	if (n >= 2 || m >= 2)
+		throw std::out_of_range("Out of range error");
+
+	return pixels[n][m];
+}
+
+#endif // PIXELQUARTET_H
