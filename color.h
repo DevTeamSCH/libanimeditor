72bc4d8495307fa409548f6db50211c0fe460da9
Levente Gyozo Lenart <3868156+fabledpig@users.noreply.github.com>
2018-09-20 01:28:07 +0200

    Color: don't depend on QColor library

--- color.h
+++ color.h
@@ -1,75 +1,94 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#include <QColor>
+#include <stdexcept>
 
-class Color : QColor {
+class Color {
+	int r, g, b, a;
+
+	inline void setComponent(int& component, int value);
+	inline static bool checkIntegrity(int value);
 public:
 	inline explicit Color(int, int, int, int = 255);
-	inline Color(const QColor&);
-	inline Color(QColor&&);
 	inline int red() const;
 	inline int green() const;
 	inline int blue() const;
+	inline int alpha() const;
 	inline void setRed(int r);
 	inline void setGreen(int g);
 	inline void setBlue(int b);
+	inline void setAlpha(int a);
 	inline Color operator+(const Color& other) const;
 	inline Color& operator+=(const Color& other);
 };
 
+void Color::setComponent(int& component, int value)
+{
+	if (!checkIntegrity(value))
+		throw std::out_of_range("Invalid color component value");
+
+	component = value;
+}
+
+bool Color::checkIntegrity(int value)
+{
+	return value >= 0 && value <= 255;
+}
+
+
 Color::Color(int r, int g, int b, int a) :
-	QColor(r, g, b, a)
-{
-
-}
-
-Color::Color(const QColor& qcolor) :
-	QColor(qcolor)
-{
-
-}
-
-Color::Color(QColor&& qcolor) :
-	QColor(std::move(qcolor))
+	r(r),
+	g(g),
+	b(b),
+	a(a)
 {
 
 }
 
 int Color::red() const
 {
-	return QColor::red();
+	return r;
 }
 
 int Color::green() const
 {
-	return QColor::green();
+	return g;
 }
 
 int Color::blue() const
 {
-	return QColor::blue();
+	return b;
+}
+
+int Color::alpha() const
+{
+	return a;
 }
 
 void Color::setRed(int r)
 {
-	QColor::setRed(r);
+	setComponent(this->r, r);
 }
 
 void Color::setGreen(int g)
 {
-	QColor::setGreen(g);
+	setComponent(this->g, g);
 }
 
 void Color::setBlue(int b)
 {
-	QColor::setBlue(b);
+	setComponent(this->b, b);
+}
+
+void Color::setAlpha(int a)
+{
+	setComponent(this->a, a);
 }
 
 Color Color::operator+(const Color& other) const
 {
 	// blend factor
-	double bf = other.alphaF();
+	double bf = other.alpha() / 255.0;
 
 	return Color(static_cast<int>((1.0 - bf) * red() + bf * other.red()),
 		     static_cast<int>((1.0 - bf) * green() +
